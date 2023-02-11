5f6e07316f65f2329dc44f0bc88fd30786f6197a
Abdul Rub <arub0419@gmail.com>
2023-02-11 17:06:36 +0530

    nav-hero

---
+++ agumentik/src/Component/Collaborate.jsx
@@ -0,0 +1,130 @@
+import React from "react";
+import style from "../styles/collaborate.module.css";
+import { BsArrowRight } from "react-icons/bs";
+
+const Collaborate = () => {
+  return (
+    <div className={style.container}>
+      {/* -------------------------------------------Upper-Half----------------------------------------- */}
+      <div className={style.upper__half}>
+        <div className={style.main__line1}>
+          <p>
+            We collaboate with{" "}
+            <span className={style.span__clr}>
+              250+ leading unversities and companies
+            </span>
+          </p>
+        </div>
+        <div className={style.cards_box}>
+          <div>
+            <img
+              src="https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-original-577x577/s3/0000/4107/brand.gif?itok=ACv3cY_Y"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              src="https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-original-577x577/s3/0000/4107/brand.gif?itok=ACv3cY_Y"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              src="https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-original-577x577/s3/0000/4107/brand.gif?itok=ACv3cY_Y"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              src="https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-original-577x577/s3/0000/4107/brand.gif?itok=ACv3cY_Y"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              src="https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-original-577x577/s3/0000/4107/brand.gif?itok=ACv3cY_Y"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+      {/* -------------------------------------------Lower-Half----------------------------------------- */}
+      <div className={style.lower__half}>
+        <div className={style.main__line2}>
+          <div className={style.h2__tag}>
+            <h2>Why Must Cousrzees ?</h2>
+          </div>
+          <div className={style.p__tag}>
+            <p>
+              We have beed doing this for more than 10 years because we want to{" "}
+              <br />
+              always provide learning service that are not taught at school
+            </p>
+          </div>
+        </div>
+        {/* ---------------------------------------------------------Cards-------------------------------- */}
+        <div className={style.div__box}>
+          <div className={style.title}>
+            <p>Flexible Time</p>
+            <hr />
+            
+            <div className={style.desc}>
+              Learning can be adjusted according to <br />
+              your time,because we also really
+              <br />
+              appreciate you if you are doing other
+              <br />
+              activities.
+              <div className={style.learn__more}>
+                Learn More{" "}
+                <span>
+                  <BsArrowRight color={"#5459E8"} size={20} />
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className={style.title}>
+            <u>
+              <p>Flexible Time</p>
+            </u>
+            <div className={style.desc}>
+              Learning can be adjusted according to <br />
+              your time,because we also really
+              <br />
+              appreciate you if you are doing other
+              <br />
+              activities.
+              <div className={style.learn__more}>
+                Learn More{" "}
+                <span>
+                  <BsArrowRight color={"#5459E8"} size={20} />
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className={style.title}>
+            <u>
+              <p>Flexible Time</p>
+            </u>
+            <div className={style.desc}>
+              Learning can be adjusted according to <br />
+              your time,because we also really
+              <br />
+              appreciate you if you are doing other
+              <br />
+              activities.
+              <div className={style.learn__more}>
+                Learn More{" "}
+                <span>
+                  <BsArrowRight color={"#5459E8"} size={20} />
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Collaborate;
--- agumentik/src/Component/Home.jsx
+++ agumentik/src/Component/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Collaborate from './Collaborate'
 import HeroPage from './HeroPage'
 import Navbar from './Navbar'
 
@@ -7,6 +8,7 @@ const Home = () => {
     <div>
         <Navbar/>
         <HeroPage/>
+        <Collaborate/>
     </div>
   )
 }
