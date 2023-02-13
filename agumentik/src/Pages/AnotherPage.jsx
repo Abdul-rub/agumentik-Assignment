e0eb51202865764fc18e9ace4437805f374c508f
Abdul Rub <arub0419@gmail.com>
2023-02-13 11:39:59 +0530

    main-page-fix

--- agumentik/src/Pages/AnotherPage.jsx
+++ agumentik/src/Pages/AnotherPage.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import Collaborate from '../Component/Collaborate';
+import ExploreCourse from '../Component/ExploreCourse';
+import HeroPage from '../Component/HeroPage';
+import Navbar from '../Component/Navbar';
+import { Pricing } from './Pricing';
+import Testimonial from './Testimonial';
+import style from "../styles/anotherPage.module.css";
 
 const AnotherPage = () => {
   return (
-    <div>AnotherPage</div>
-  )
-}
+    <>
+      <div className={style.container}>
+        <div className={style.back}>
+          <ExploreCourse />
+          <Pricing />
+          <Testimonial />
+        </div>
+        <div className={style.front}>
+          <Navbar />
+          <HeroPage />
+          <Collaborate />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default AnotherPage
+export default AnotherPage;
